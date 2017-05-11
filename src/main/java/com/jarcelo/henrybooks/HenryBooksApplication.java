7fb5007bec0befe60878d53f838a83098e32d507
Joe Arcelo <josepharcelo@gmail.com>
2017-05-11 15:01:49 -0500

    first commit

---
+++ src/main/java/com/jarcelo/henrybooks/HenryBooksApplication.java
@@ -0,0 +1,12 @@
+package com.jarcelo.henrybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenryBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HenryBooksApplication.class, args);
+	}
+}
