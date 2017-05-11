058c4dcbd54be26c9ca588aaba08734c4b9ee88c
Joe Arcelo <josepharcelo@gmail.com>
2017-05-11 17:12:54 -0500

    Add book controller

---
+++ src/main/java/com/jarcelo/henrybooks/controllers/BookController.java
@@ -0,0 +1,27 @@
+package com.jarcelo.henrybooks.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jarcelo.henrybooks.models.BookDao;
+
+@Controller
+public class BookController {
+	@Autowired
+	private BookDao bookDao;
+	
+	@RequestMapping("/book/count")
+	@ResponseBody
+	public String count() {
+		long bookCount;
+		try {
+			bookCount = bookDao.count();
+		} catch (Exception e) {
+			return "Error retrieving book count " + e.getMessage();
+		}
+		return "Number of books: " + bookCount;
+	}
+	
+}
